e35b45b5fddc90169393186e0a7702e59ba01120
Chris Heyes <cdheyes@outlook.com>
2022-04-20 16:16:19 +0100

    v3

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
-	const [cats, setCat] = useState([]);
-	const [error, setError] = useState(null);
-
-	useEffect(() => {
-		const fetchCats = async () => {
-			try {
-				const response = await fetch(
-					'https://api.thecatapi.com/v1/images/search',
-				);
-				if (!response.ok) {
-					throw new Error(response.statusText);
-				}
-
-				const data = await response.json();
-				setCat(data);
-			} catch (err) {
-				setError('Could not fetch data');
-				console.log(err.message);
-			}
-		};
-		fetchCats();
-	}, []);
-
-	return (
-		<div className="App">
-			{cats.map((cat, index) => (
-				<div key={index}>
-					{error && <p>{error}</p>}
-          <h2>{cat.id}</h2>
-					<img src={cat.url} alt="cat mug shot" />
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navbar />
+     <div className="content">
+       <Hero />
+     </div>
+    </div>
+  );
 }
 
 export default App;
