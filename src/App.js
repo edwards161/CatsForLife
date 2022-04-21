c52fc18f4e5092f75ff3411d9ea40d0776eed58d
arician <arician@live.com>
2022-04-21 16:38:38 +0100

    changes to api structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import Hero from './components/Hero';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { useEffect, useState } from 'react';
+import GetImage from './utils/GetImages';
+import GetData from './utils/GetData';
 
 
 
 
 
 function App() {
+  const [catData, setCatData] = useState([]);
 
+  useEffect(() => {
+    (async () => {
+      const imgs = await GetImage();
+      let data = GetData();
+      data = data.map((cat, i) => {
+        cat.image = imgs[i].url;
+        cat.id = i;
+        return cat;
+      });
+      setCatData(data);
+    })();
+  }, []);
 
   return (
     <div className="App">
       <Navbar />
      <div className="content">
-       <Hero />
+       <Home catData={catData}/>
      </div>
      <Footer />
     </div>
---
+++ src/utils/GetImages.js
@@ -0,0 +1,17 @@
+const GetImage = async () => {
+	try {
+		const response = await fetch(
+			'https://api.thecatapi.com/v1/images/search?limit=4',
+		);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+		const res = await response.json();
+		return res;
+	} catch (err) {
+		console.error(`${err.message}`);
+	}
+};
+
+export default GetImage;
